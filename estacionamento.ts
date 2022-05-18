f41c1746825b1c9a4dc253132b2fe659ea4117d6
Pedro Leone <pedroleone@gmail.com>
2022-05-17 21:38:45 -0300

    Logica de cadastro de veiculos com bug

--- estacionamento.ts
+++ estacionamento.ts
@@ -28,3 +28,82 @@ function controlSectionDisplay(childSectionName: string) {
         element.style.display = "none";
     }
 }
+
+type corVeiculo = "preto" | "branco" | "prata" | "azul" | "verde" | "vermelho";
+type tipoVeiculo = "small-car" | "large-car" | "motorcycle";
+
+class Veiculo {
+    readonly nome: string;
+    readonly placa: string;
+    readonly tipo: tipoVeiculo;
+    readonly entrada: Date ;
+    readonly cor: corVeiculo;
+    readonly mensalista?: boolean;
+    saida?: Date | string;
+    valorPago?: number ;
+    
+
+    constructor(nome: string, placa: string, tipo: string, entrada: Date, cor: string) {
+        this.nome = nome;
+        this.placa = placa;
+        this.tipo = tipo as tipoVeiculo;
+        this.entrada = entrada;
+        this.cor = cor as corVeiculo;
+    }
+
+    renderizaHtml(): string {
+        return `
+        <div class="park-item">
+            <div class="item-img">
+                <img src="./assets/${this.tipo}.svg" alt="${this.tipo}" width="95" height="80">
+            </div>
+            <div class="item-ident">
+                <div class="item-placa-cor">
+                    <div class="item-placa">${this.placa}</div>
+                    <div class="placa ${this.cor}"></div>
+                </div>
+                <div class="item-modelo">${this.nome}</div>
+                <div class="item-datahora">${this.entrada.toISOString()}</div>
+            </div>
+            <div class="item-botoes">
+                <div class="botao"><img src="./assets/delete.svg" class="btn-deletar" data-placa="${this.placa}" width="45" height="45"></div>
+                <div class="botao"><img src="./assets/checkout.svg" class="btn-checkout" data-placa="${this.placa}" width="45" height="45"></div>
+            </div>
+        </div>
+        `;
+    }    
+}
+
+// const veiculo1 = new Veiculo('Ford Fiesta','FRA7415','small-car',new Date,"prata");
+// const veiculo2 = new Veiculo('Jeep Renegade','ABC1234','large-car',new Date,"verde");
+// const veiculo3 = new Veiculo('Yamaha R3','ABC1234','motorcycle',new Date,"preto");
+
+
+let listaVeiculos: Array<Veiculo> = [];
+
+const $ = (query: string): HTMLInputElement | null => document.querySelector(query);
+
+document.querySelector("#btn-cadastrar").addEventListener("click", function (e) {
+    e.preventDefault();
+    // nome: string, placa: string, tipo: string, entrada: Date, cor: string
+    const nome = $("#modelo").value;
+    const placa = $("#placa").value;
+    const tipo = $("input[name='cartype']:checked").value;
+    const entrada = new Date;
+    const corOption = document.querySelector("#cor") as HTMLSelectElement;
+    const cor = corOption.value;
+    const veiculo = new Veiculo(nome, placa, tipo, entrada, cor);
+    listaVeiculos.push(veiculo);
+    renderizaListaVeiculos(listaVeiculos);
+});
+
+
+
+function renderizaListaVeiculos (listaVeiculos: Veiculo[]) {
+    let listaVeiculosHTML: string;
+    listaVeiculos.forEach((veiculo) => listaVeiculosHTML += veiculo.renderizaHtml());
+    const container = document.querySelector(".park-container")
+    let item = document.createElement("div");
+    item.innerHTML = listaVeiculosHTML;
+    container.appendChild(item);
+}
