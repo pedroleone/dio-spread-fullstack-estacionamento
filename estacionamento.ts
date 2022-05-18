19268dacf4b078e4f325043ac1e5668fb272bee5
Pedro Leone <pedroleone@gmail.com>
2022-05-18 11:06:40 -0300

    Objeto Patio adicionando novos veiculos

--- estacionamento.ts
+++ estacionamento.ts
@@ -40,7 +40,7 @@ class Veiculo {
     readonly cor: corVeiculo;
     readonly mensalista?: boolean;
     saida?: Date | string;
-    valorPago?: number ;
+    valorPago?: number;
     
 
     constructor(nome: string, placa: string, tipo: string, entrada: Date, cor: string, mensalista: boolean) {
@@ -74,9 +74,129 @@ class Veiculo {
     }    
 }
 
-let listaVeiculos: Array<Veiculo> = [];
-
 const $ = (query: string): HTMLInputElement | null => document.querySelector(query);
+
+
+
+
+type configuracoesVeiculo = {
+    primeiraHora: number;
+    segundaHora: number;
+    demaisHoras: number;
+    capacidade: number;
+}
+
+class Configuracao {
+    carroPequeno: configuracoesVeiculo;
+    carroGrande: configuracoesVeiculo;
+    motocicleta: configuracoesVeiculo;
+
+    constructor(carroPequeno: configuracoesVeiculo = {
+            primeiraHora: 6,
+            segundaHora: 4.5,
+            demaisHoras: 4,
+            capacidade: 50
+        }, 
+        carroGrande: configuracoesVeiculo = {
+            primeiraHora: 12,
+            segundaHora: 7,
+            demaisHoras: 5,
+            capacidade: 10
+            },
+        motocicleta: configuracoesVeiculo  = {
+            primeiraHora: 4,
+            segundaHora: 2.5,
+            demaisHoras: 2.5,
+            capacidade: 25
+            }) {
+        this.carroPequeno = carroPequeno;
+        this.carroGrande = carroGrande;
+        this.motocicleta = motocicleta;
+    }
+
+    renderizarControles() {
+        $("#primeirahora-peq").value = this.carroPequeno.primeiraHora.toLocaleString('pt-BR');
+        $("#segundahora-peq").value = this.carroPequeno.segundaHora.toLocaleString('pt-BR');
+        $("#demaishoras-peq").value = this.carroPequeno.demaisHoras.toLocaleString('pt-BR');
+        $("#capacidade-peq").value = this.carroPequeno.capacidade.toString();
+
+        $("#primeirahora-gde").value = this.carroGrande.primeiraHora.toLocaleString('pt-BR');
+        $("#segundahora-gde").value = this.carroGrande.segundaHora.toLocaleString('pt-BR');
+        $("#demaishoras-gde").value = this.carroGrande.demaisHoras.toLocaleString('pt-BR');
+        $("#capacidade-gde").value = this.carroGrande.capacidade.toString();
+
+        $("#primeirahora-moto").value = this.motocicleta.primeiraHora.toLocaleString('pt-BR');
+        $("#segundahora-moto").value = this.motocicleta.segundaHora.toLocaleString('pt-BR');
+        $("#demaishoras-moto").value = this.motocicleta.demaisHoras.toLocaleString('pt-BR');
+        $("#capacidade-moto").value = this.motocicleta.capacidade.toString();
+    }
+}
+
+class Patio {
+    configuracao: Configuracao;
+    veiculosPatio: Array<Veiculo>;
+    ocupacao: {
+        carroPequeno: number,
+        carroGrande: number,
+        motocicleta: number
+    }
+    veiculosFaturados: Array<Veiculo>;
+
+    constructor() {
+        this.configuracao = new Configuracao();
+        this.configuracao.renderizarControles();
+        this.veiculosPatio = [];
+        this.ocupacao = {
+            carroPequeno: 0,
+            carroGrande: 0,
+            motocicleta: 0
+        }
+        this.calcularOcupacao();
+        this.atualizaDados();
+        
+    }
+
+    adicionaVeiculo(veiculo: Veiculo) {
+        this.veiculosPatio.push(veiculo);
+        this.calcularOcupacao();
+        this.atualizaDados();
+        this.renderizaPatio();
+    }
+
+    renderizaPatio() {
+        let listaVeiculosHTML: string = "";
+        this.veiculosPatio.forEach((veiculo) => listaVeiculosHTML += veiculo.renderizaHtml());
+        const container = document.querySelector(".park-container")
+        container.innerHTML= listaVeiculosHTML;        
+    }
+
+    calcularOcupacao() {
+        this.ocupacao.carroPequeno = 0;
+        this.ocupacao.carroGrande = 0;
+        this.ocupacao.motocicleta = 0;
+        this.veiculosPatio.forEach((veiculo) => {
+            if (veiculo.tipo === 'small-car') this.ocupacao.carroPequeno++;
+            if (veiculo.tipo === 'large-car') this.ocupacao.carroGrande++;
+            if (veiculo.tipo === 'motorcycle') this.ocupacao.motocicleta++;
+        })
+
+    }
+
+    atualizaDados() {
+        $("#ocupacao-peq").value = this.ocupacao.carroPequeno.toString();
+        $("#ocupacao-gde").value = this.ocupacao.carroGrande.toString();
+        $("#ocupacao-moto").value = this.ocupacao.motocicleta.toString();
+
+        $("#vagas-peq").value = (this.configuracao.carroPequeno.capacidade - this.ocupacao.carroPequeno).toString();
+        $("#vagas-gde").value = (this.configuracao.carroGrande.capacidade - this.ocupacao.carroGrande).toString();
+        $("#vagas-moto").value = (this.configuracao.motocicleta.capacidade - this.ocupacao.motocicleta).toString();
+
+    }
+}
+
+
+const patio = new Patio();
+
 
 document.querySelector("#btn-cadastrar").addEventListener("click", function (e) {
     e.preventDefault();
@@ -89,15 +209,6 @@ document.querySelector("#btn-cadastrar").addEventListener("click", function (e) 
     const mensalista = $("#mensalista").checked;
     const veiculo = new Veiculo(nome, placa, tipo, entrada, cor, mensalista);
     
-    listaVeiculos.push(veiculo);
-    renderizaListaVeiculos(listaVeiculos);
+    patio.adicionaVeiculo(veiculo);
+    
 });
-
-
-
-function renderizaListaVeiculos (listaVeiculos: Veiculo[]) {
-    let listaVeiculosHTML: string = "";
-    listaVeiculos.forEach((veiculo) => listaVeiculosHTML += veiculo.renderizaHtml());
-    const container = document.querySelector(".park-container")
-    container.innerHTML= listaVeiculosHTML;
-}
